63fb2213a01bf05a237347f1e66537780657ef51
AntonKulik1980 <40906825+AntonKulik1980@users.noreply.github.com>
2020-12-13 08:12:44 +0100

    12132020

--- shop/mosels/extra_models.py
+++ shop/mosels/extra_models.py
@@ -7,3 +7,9 @@ class News(Time_stamp):
     title = me.StringField(required=True,min_length=2,max_length=256)
     body = me.StringField(required=True,min_length=2,max_length=2048)
     # TODO datetime class
+
+
+
+
+
+
